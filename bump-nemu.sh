2f6e26966af66d7c57263d14f980374e4553df9c
yu-yake2002 <yuyake02@outlook.com>
2026-02-27 11:30:15 +0800

    Bump nemu refs for XSAI

--- bump-nemu.sh
+++ bump-nemu.sh
@@ -4,16 +4,16 @@ if [ ! $NEMU_HOME ]; then
 else
   echo "Use $NEMU_HOME to bump NEMU"
   rm riscv64-nemu-interpreter-so
-  # rm riscv64-nemu-interpreter-dual-so
+  rm riscv64-nemu-interpreter-dual-so
   cd $NEMU_HOME
   make clean
   make riscv64-matrix-xs-ref_defconfig
   make -j
   cp build/riscv64-nemu-interpreter-so ${ready_to_run_home}
-  # make clean
-  # make riscv64-dual-xs-ref_defconfig
-  # make -j
-  # cp build/riscv64-nemu-interpreter-so ${ready_to_run_home}/riscv64-nemu-interpreter-dual-so
+  make clean
+  make riscv64-dual-matrix-xs-ref_defconfig
+  make -j
+  cp build/riscv64-nemu-interpreter-so ${ready_to_run_home}/riscv64-nemu-interpreter-dual-so
   cd ${ready_to_run_home}
   echo ${ready_to_run_home} "has been updated"
 fi
